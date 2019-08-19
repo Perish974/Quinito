eec2a3a78364121c55f83583c0e970dd1be82027
Perish974 <53513901+Perish974@users.noreply.github.com>
2019-08-19 14:41:05 +0200

    Add files via upload

---
+++ ConsolePendu.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pendu
+{
+    public partial class Pendu : Form
+    {
+        public Pendu()
+        {
+            InitializeComponent();
+        }
+
+        private void A_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void B_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void D_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void E_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void F_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void G_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void H_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void I_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void J_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void K_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void L_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void M_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void N_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void O_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void P_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Q_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void R_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void S_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void T_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void U_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void V_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void W_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void X_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Y_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Z_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tiret_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumeroManche_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Mancheencours_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Nombreessais_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+               
+        private void Rejouer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Quitter_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
